019988b99e1a4dc11be91289b7dc30e7efacc5b6
adelakim <diddnjs02@likelion.org>
2020-10-29 17:08:26 +0900

    correct: 1052
    백준 1052번 문제 정답 소스코드 및 풀이

---
+++ 시뮬레이션/물병/물병.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+// let [n, k] = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map((e) => +e);
+const [n, k] = fs
+  .readFileSync(__dirname + "/stdin.txt")
+  .toString()
+  .replace(/\r/g, "")
+  .trim()
+  .split(" ")
+  .map((e) => +e);
+
+if (n <= k) {
+  console.log(-1);
+  return;
+}
+
+function getCount(n) {
+  let cnt = 0;
+  while (n > 0) {
+    if (n % 2 !== 0) cnt++;
+    n = Math.floor(n / 2);
+  }
+  return cnt;
+}
+
+let ans = n;
+while (true) {
+  if (getCount(ans) <= k) break;
+  else ans++;
+}
+
+console.log(ans - n);
