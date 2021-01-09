61fe041ef835377b53fae4773637d07c60cf050b
adelakim <diddnjs02@likelion.org>
2021-01-10 00:19:21 +0900

    correct: 1106
    백준 1106번 문제 정답 소스코드 및 풀이

---
+++ 구현/호텔/main.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const [c, n] = input[0].split(" ").map((e) => +e);
+let customers = input.slice(1).map((e) => e.split(" ").map((i) => +i));
+customers.sort((a, b) => a[0] - b[0]);
+
+let dp = Array(c + 1).fill(Infinity);
+dp[0] = 0;
+
+for (let [cost, customN] of customers) {
+  if (dp[customN] > cost) dp[customN] = cost;
+  for (let i = 1; i <= c; i++) {
+    dp[i] = i < customN ? Math.min(dp[i], cost) : Math.min(dp[i], dp[customN] + dp[i - customN]);
+  }
+}
+
+console.log(dp[c]);
